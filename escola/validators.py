d359c71533c49b6825e8b1318c64aff257d9f895
Pedro Henrique Ernandes da Silva <thepedrinhoo@gmail.com>
2024-10-08 17:54:10 -0300

    arquivo de validacao

--- escola/validators.py
+++ escola/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_invalido(cpf):
     return len(cpf) != 11
 
@@ -5,18 +7,7 @@ def nome_invalido(nome):
     return not nome.isalpha()
 
 def celular_invalido(celular):
-    return len(celular) != 13
-
-## Criando método para validações de CPF, nome, celular ##
-
-    def validate(self, dados):
-        if len(dados['cpf']) != 11:
-            raise serializers.ValidationError({'CPF': 'O CPF deve ter 11 dígitos.'})
-        
-        if not dados['nome'].isalpha():
-            raise serializers.ValidationError({'NOME': 'O nome só pode ter letras.'})
-        
-        if len(dados['celular']) != 13:
-            raise serializers.ValidationError({'CELULAR:' 'O celular precisa ter 13 dígitos.'})
-    
-        return dados
+    #86 99999-9999
+    modelo = '[0-9]{2} [0-9]{5}-[0-9{4}]'
+    resposta = re.findall(modelo, celular)
+    return not resposta # caso não for um celular válido
